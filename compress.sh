3f84af6e8c0177bbf8db5f6c89cf24efb81d218d
NicHub <nicolas.jeanmonod@gmail.com>
2018-02-18 14:37:29 +0100

    modif compress.sh

--- compress.sh
+++ compress.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# Pour utiliser ce script
+# cd fontawesome-local
+# ou Git Bash Here dans l’explorateur Windows
+# ./compress.sh
+
 # Pour compresser des fichiers un par un.
-gzip --keep                           \
+gzip -9 --keep --force                \
   fontawesomel-used-icons-only.min.js \
   fontawesomel-used-icons-only.js
 
 # Pour compresser tous les fichiers JavaScript.
-# gzip --keep *.js
+# gzip -9 --keep --force *.js
